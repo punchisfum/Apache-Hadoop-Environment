135c511e4a415cfac34120980f265ed9125412b2
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-08-28 16:40:43 +0700

    Clean Scripts

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
+echo "";
 
-echo "################################################"
-echo "##  Welcom To Bayu Dwiyan Satria Installation ##"
-echo "################################################"
+echo "################################################";
+echo "##  Welcom To Bayu Dwiyan Satria Installation ##";
+echo "################################################";
 
-echo ""
+echo "";
 
-echo "Use of code or any part of it is strictly prohibited."
-echo "File protected by copyright law and provided under license."
-echo "To Use any part of this code you need to get a writen approval from the code owner: bayudwiyansatria@gmail.com."
+echo "Use of code or any part of it is strictly prohibited.";
+echo "File protected by copyright law and provided under license.";
+echo "To Use any part of this code you need to get a writen approval from the code owner: bayudwiyansatria@gmail.com.";
 
-echo ""
+echo "";
 
 # User Access
 if [ $(id -u) -eq 0 ]; then
 
-    echo "################################"
-    echo "##  System Compability Check  ##"
-    echo "################################"
+    echo "################################################";
+    echo "##        Checking System Compability         ##";
+    echo "################################################";
 
+    echo "";
     echo "Please wait! Checking System Compability";
+    echo "";
+
     argv="$1";
     echo $argv;
     if [ "$argv" ] ; then
@@ -30,7 +34,7 @@ if [ $(id -u) -eq 0 ]; then
         if [ -z "$version" ] ; then 
             echo "Hadoop version is not specified! Installing hadoop with lastest stable version";
             distribution="stable";
-            version="3.2.0"
+            version="3.2.0";
             packages="hadoop-$version";
         else
             distribution="hadoop-$version";
@@ -39,12 +43,12 @@ if [ $(id -u) -eq 0 ]; then
     fi
 
     # Packages Available
-    url=https://www-eu.apache.org/dist/hadoop/common/$distribution/$packages.tar.gz
+    url=https://www-eu.apache.org/dist/hadoop/common/$distribution/$packages.tar.gz;
     echo "Checking availablility hadoop $version";
     if curl --output /dev/null --silent --head --fail "$url"; then
-        echo "Hadoop version is available: $url"
+        echo "Hadoop version is available: $url";
     else
-        echo "Hadoop version isn't available: $url"
+        echo "Hadoop version isn't available: $url";
         exit 1;
     fi
 
@@ -54,33 +58,32 @@ if [ $(id -u) -eq 0 ]; then
 
     # System Operation Information
     if type lsb_release >/dev/null 2>&1 ; then
-    os=$(lsb_release -i -s)
+    os=$(lsb_release -i -s);
     elif [ -e /etc/os-release ] ; then
-    os=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
+    os=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release);
     elif [ -e /etc/*-os-release ] ; then
-    os=$(awk -F= '$1 == "ID" {print $3}' /etc/*-os-release)
+    os=$(awk -F= '$1 == "ID" {print $3}' /etc/*-os-release);
     fi
 
-    os=$(printf '%s\n' "$os" | LC_ALL=C tr '[:upper:]' '[:lower:]')
+    os=$(printf '%s\n' "$os" | LC_ALL=C tr '[:upper:]' '[:lower:]');
 
-    read -p "Update Distro (y/n) [ENTER] (y)(Recommended): " update
+    read -p "Update Distro (y/n) [ENTER] (y)(Recommended): " update;
     if [ $update == "y" ] ; then
         if [ $os == "ubuntu" ] ; then
-            apt-get -y update && apt-get -y upgrade
+            apt-get -y update && apt-get -y upgrade;
         else 
-            yum -y update && yum -y upgrade
+            yum -y update && yum -y upgrade;
         fi
     fi
 
     if [ $os == "ubuntu" ] ; then
-        apt-get -y install git && apt-get -y install wget
+        apt-get -y install git && apt-get -y install wget;
     else 
-        yum -y install git && yum -y install wget
+        yum -y install git && yum -y install wget;
     fi
 
-    echo "################################"
-    echo "## Installing Hadoop Version  ##"
-    echo "################################"
+    echo "Installing Hadoop Version  $distribution";
+    echo "";
 
     # Extraction Packages
     tar -xvf /tmp/$packages.tar.gz;
@@ -88,9 +91,9 @@ if [ $(id -u) -eq 0 ]; then
     mv /tmp/$packages /usr/local/hadoop;
 
     # User Generator
-    read -p "Enter username : " username
-    read -s -p "Enter password : " password
-    egrep "^$username" /etc/passwd >/dev/null
+    read -p "Enter username : " username;
+    read -s -p "Enter password : " password;
+    egrep "^$username" /etc/passwd >/dev/null;
     if [ $? -eq 0 ]; then
         echo "$username exists!"
     else
@@ -99,27 +102,27 @@ if [ $(id -u) -eq 0 ]; then
         [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
     fi
 
-    usermod -aG $username $password
-    chown $username:root -R /usr/local/hadoop
-    chmod g+rwx -R /usr/local/hadoop
+    usermod -aG $username $password;
+    chown $username:root -R /usr/local/hadoop;
+    chmod g+rwx -R /usr/local/hadoop;
 
-    echo "################################"
-    echo "## Hadoop Configuration Setup ##"
-    echo "################################"
+    echo "################################";
+    echo "## Hadoop Configuration Setup ##";
+    echo "################################";
+    echo "";
 
-    read -p "Using default configuration (y/n) [ENTER] (y): " conf
+    read -p "Using default configuration (y/n) [ENTER] (y): " conf;
     if $conf == "y" ; then
         # Configuration Variable
         configuration=(core-site.xml hdfs-site.xml httpfs-site.xml kms-site.xml mapred-site.xml yarn-site.xml);
-        for xml in "${configuration[@]}"
-        do 
+        for xml in "${configuration[@]}" ; do 
             wget https://raw.githubusercontent.com/bayudwiyansatria/Apache-Hadoop-Environment/master/$packages/etc/hadoop/$xml -O /tmp/$xml;
         done
     fi
 
-    echo "############################################"
-    echo "## Thank You For Using Bayu Dwiyan Satria ##"
-    echo "############################################"
+    echo "############################################";
+    echo "## Thank You For Using Bayu Dwiyan Satria ##";
+    echo "############################################";
 
     echo "Installed Directory /usr/local/hadoop";
     echo "Installing Hadoop $version Successfully";
@@ -127,6 +130,6 @@ if [ $(id -u) -eq 0 ]; then
     echo "Pass $password";
 
 else
-    echo "Only root may add a user to the system"
+    echo "Only root may add a user to the system";
     exit 2;
 fi
