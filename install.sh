d0eaec80f97251c5412b25cea6a5de5375c67df4
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-09-09 15:28:42 +0700

    SSH AUTH

--- install.sh
+++ install.sh
@@ -350,7 +350,7 @@ if [ $(id -u) -eq 0 ]; then
                 ssh $worker "wget https://raw.githubusercontent.com/bayudwiyansatria/Apache-Hadoop-Environment/master/express-install.sh";
                 ssh $worker "chmod 777 express-install.sh";
                 ssh $worker "./express-install.sh" $version "http://bdev.bayudwiyansatria.com/dist/hadoop" "$username" "$password";
-                sudo -i -u $username bash -c "ssh-copy-id -i /home/$username/.ssh/id_rsa.pub $worker;
+                sudo -i -u $username bash -c "ssh-copy-id -i /home/$username/.ssh/id_rsa.pub $worker";
                 ssh $worker "echo -e  ''$ipaddr' # Master' >> $HADOOP_HOME";
                 read -p "Do you want to add more worker? (y/N) [ENTER] (n) " workeraccept;
                 workeraccept=$(printf '%s\n' "$workeraccept" | LC_ALL=C tr '[:upper:]' '[:lower:]' | sed 's/"//g'); 
