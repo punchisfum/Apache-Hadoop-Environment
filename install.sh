7777796169f5d98f7397d6baa926b4c1b28c3516
Bayu Dwiyan Satria <bayudwiyansatria@gmail.com>
2019-09-11 13:29:05 +0700

    Worker conf

--- install.sh
+++ install.sh
@@ -427,7 +427,7 @@ if [ $(id -u) -eq 0 ]; then
             if [ "$workeraccept" == "y" ] ; then 
                 while [ "$workeraccept" == "y" ] ; do 
                     read -p "Please enter worker IP Address [ENTER] " worker;
-                    echo -e  ''$worker' # Worker' >> $HADOOP_HOME/etc/worker;
+                    echo -e  ''$worker' # Worker' >> $HADOOP_HOME/etc/hadoop/worker;
                     if [[ -f "~/.ssh/id_rsa" && -f "~/.ssh/id_rsa.pub" ]]; then 
                         echo "SSH already setup";
                         echo "";
